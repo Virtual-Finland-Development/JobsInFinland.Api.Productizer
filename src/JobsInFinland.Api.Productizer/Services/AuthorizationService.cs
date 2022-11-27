fba29e90fe6b299e4275ec417378f545526b5455
lassi.patanen <lassi.patanen@gofore.com>
2022-11-27 14:58:18 +0200

    Clean up request message

--- src/JobsInFinland.Api.Productizer/Services/AuthorizationService.cs
+++ src/JobsInFinland.Api.Productizer/Services/AuthorizationService.cs
@@ -17,19 +17,12 @@ internal class AuthorizationService : IAuthorizationService
             x => x.Key.ToLowerInvariant(),
             x => x.Value.ToString());
 
-        var authorizationRequest = new HttpRequestMessage
-        {
-            RequestUri = new Uri($"{_client.BaseAddress}authorize"),
-            Method = HttpMethod.Post,
-            Headers =
-            {
-                { "authorization", headers["authorization"] },
-                { "x-authorization-provider", headers["x-authorization-provider"] },
-                { "x-authorization-context", "TMT-productizer" }
-            }
-        };
+        using var requestMessage = new HttpRequestMessage(HttpMethod.Post, _client.BaseAddress + "authorize");
+        requestMessage.Headers.Add("authorization", headers["authorization"]);
+        requestMessage.Headers.Add("x-authorization-provider", headers["x-authorization-provider"]);
+        requestMessage.Headers.Add("x-authorization-context", "jif-productizer");
 
-        var result = await _client.SendAsync(authorizationRequest);
+        var result = await _client.SendAsync(requestMessage);
         if (!result.IsSuccessStatusCode)
             throw new HttpRequestException("Access Denied", null, HttpStatusCode.Unauthorized);
     }
