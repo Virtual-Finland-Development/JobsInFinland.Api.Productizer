4a0d15616bf498c689e21f964e7c13695085dc1c
Lassi Piironen <lsipii@kapsi.fi>
2022-11-28 13:44:59 +0200

    feat: drop the x-authorization-provider header from requirements

--- src/JobsInFinland.Api.Productizer/Services/AuthorizationService.cs
+++ src/JobsInFinland.Api.Productizer/Services/AuthorizationService.cs
@@ -18,15 +18,11 @@ internal class AuthorizationService : IAuthorizationService
             x => x.Value.ToString());
 
         headers.TryGetValue("authorization", out var authorization);
-        headers.TryGetValue("x-authorization-provider", out var authorizationProvider);
 
         using var requestMessage = new HttpRequestMessage(HttpMethod.Post, _client.BaseAddress + "authorize");
 
         requestMessage.Headers.Add("authorization",
             authorization ?? throw new InvalidOperationException("Missing header: Authorization"));
-
-        requestMessage.Headers.Add("x-authorization-provider",
-            authorizationProvider ?? throw new InvalidOperationException("Missing Header: X-Authorization-Provider"));
 
         requestMessage.Headers.Add("x-authorization-context", "jif-productizer");
 
