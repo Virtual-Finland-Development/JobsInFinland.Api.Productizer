a05a3ed4d0db863527f6e017cf2587182af47866
lassi.patanen <lassi.patanen@gofore.com>
2022-11-30 09:33:35 +0200

    Get AuthGW API base address from either appsettings file or from environment variable

--- src/JobsInFinland.Api.Productizer/Program.cs
+++ src/JobsInFinland.Api.Productizer/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<IAuthorizationService, AuthorizationService>(client =>
     client.BaseAddress = new Uri(
         builder.Configuration.GetSection("AuthGwBaseAddress").Value ??
-        Environment.GetEnvironmentVariable("AuthGwBaseAddress") ??
+        Environment.GetEnvironmentVariable("AUTH_GW_BASE_ADDRESS") ??
         throw new InvalidOperationException("Missing configuration value for Auth GW API base address")
     )
 );
