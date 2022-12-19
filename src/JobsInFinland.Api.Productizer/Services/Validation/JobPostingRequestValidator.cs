76d5bfa1664105454594a635998754638e230423
lassi.patanen <lassi.patanen@gofore.com>
2022-12-19 17:06:38 +0200

    Load codesets in validator to check if supplied municipality and occupation codes are valid

--- src/JobsInFinland.Api.Productizer/Services/Validation/JobPostingRequestValidator.cs
+++ src/JobsInFinland.Api.Productizer/Services/Validation/JobPostingRequestValidator.cs
@@ -1,11 +1,12 @@
 using FluentValidation;
 using JobsInFinland.Api.Productizer.Models.Testbed;
+using JobsInFinland.Api.Productizer.Services.Codeset;
 
 namespace JobsInFinland.Api.Productizer.Services.Validation;
 
 public class JobPostingRequestValidator : AbstractValidator<JobsPostingRequest>
 {
-    public JobPostingRequestValidator()
+    public JobPostingRequestValidator(IOccupationCodesetService occupationCodesetService, IMunicipalityCodesetService municipalityCodesetService)
     {
         RuleFor(x => x.Query).MaximumLength(2048);
         RuleForEach(x => x.Location.Countries).MaximumLength(2);
@@ -15,6 +16,16 @@ public class JobPostingRequestValidator : AbstractValidator<JobsPostingRequest>
         const string occupationUriPattern = "http[s]?:////data.europa.eu//esco//((isco)|(occupation))//((C[0-9]*)|([0-9A-Fa-f]{8}-([0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12}))";
         RuleForEach(x => x.Requirements.Occupations).Matches(occupationUriPattern);
         */
+
+        var municipalityCodeset = municipalityCodesetService.GetCodeset();
+        RuleForEach(x => x.Location.Municipalities)
+            .Must(x => municipalityCodeset.ContainsKey(x))
+            .WithMessage(x => "Municipality code was incorrect and could not be mapped to name");
+        
+        var occupationCodeset = occupationCodesetService.GetCodeset();
+        RuleForEach(x => x.Requirements.Occupations)
+            .Must(x => occupationCodeset.ContainsKey(x))
+            .WithMessage(x => "Occupation code was incorrect and could not be mapped to name");
 
         RuleForEach(x => x.Location.Municipalities).Length(3);
         RuleForEach(x => x.Location.Countries).Length(2);
